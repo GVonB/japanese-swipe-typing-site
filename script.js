65188ff878f05be72f1880e6938fce1b6204c8f3
Gunnar Von Bergen <vonbergb@miamioh.edu>
2024-03-10 23:24:15 -0400

    Fix styling and preview
    Utilizing placeholder of text-input for prompt instead of another box

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const inputArea = document.getElementById('input-area');
-    const displayCharacter = document.getElementById('display-character');
     const hiraganaCharacters = [
         'あ', 'い', 'う', 'え', 'お',
         'か', 'き', 'く', 'け', 'こ',
@@ -17,16 +16,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function setRandomCharacter() {
         const randomIndex = Math.floor(Math.random() * hiraganaCharacters.length);
-        displayCharacter.textContent = hiraganaCharacters[randomIndex];
+        inputArea.placeholder = hiraganaCharacters[randomIndex];
     }
 
     setRandomCharacter(); // Set an initial random character
 
     inputArea.addEventListener('input', () => {
-        if (inputArea.value === displayCharacter.textContent) {
+        if (inputArea.value === inputArea.placeholder) {
             setRandomCharacter(); // Display a new character upon correct input
             inputArea.value = ''; // Clear input field after correct entry
-        } else if (inputArea.value.length >= 2) {
+        } else if (inputArea.value.length >= 3) { // 3 chars allowed for 「し」 on romaji keyboard
             inputArea.value = ''; // Clear input if incorrect (ensures single character input)
         }
     });
